db9cb04a7bf0adb8a894e6c2c689de0e4ae00fc2
aakoch <github2@dogtired.org>
2024-02-04 20:08:24 -0600

    saving progress

--- index.js
+++
@@ -1,8 +0,0 @@
-var ssi = require("ssi");
-
-var inputDirectory = "public";
-var outputDirectory = "output";
-var matcher = "**/*.shtml";
-
-var includes = new ssi(inputDirectory, outputDirectory, matcher);
-includes.compile();
