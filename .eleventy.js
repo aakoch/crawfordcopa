750c73e8f782efac887e5fffc416b9e728829d90
aakoch <github2@dogtired.org>
2024-02-20 16:38:59 -0600

    added 11ty
    Signed-off-by: aakoch <github2@dogtired.org>

---
+++ .eleventy.js
@@ -0,0 +1,18 @@
+module.exports = function(eleventyConfig) {
+    // tells 11ty to just copy files from src to dist
+    eleventyConfig.addPassthroughCopy("src/");
+    // I thought it told 11ty to not change .html files to <filename>/index.html, but I'm having better luck with setting 
+    // the templateFormats below to "md"
+    eleventyConfig.setDynamicPermalinks(false);
+    
+    return {
+        // tells 11ty to ignore .html files as template files
+        templateFormats: ["md"],
+        dir: {
+            // 11ty uses . as the default input
+            input: "src",
+            // and _site as the default output
+            output: "dist"
+        }
+    }
+};
